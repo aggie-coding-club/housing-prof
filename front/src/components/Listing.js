2429bc556d40761bdaca96a6a37b5c694f74b5e0
jmnelson02 <josh.nelson71@yahoo.com>
2022-11-11 15:27:08 -0600

    move avail. indicator to card col 2

--- front/src/components/Listing.js
+++ front/src/components/Listing.js
@@ -6,17 +6,20 @@ export default function Listing(props) {
     <div className="card-container">
         <div className="card-column one">
           <h3>{props.complex}</h3>
-          <div className={props.available ? "availability-toggle available" : "availability-toggle unavailable"}>
-            {props.available ? "Available" : "Unavailable"}
-          </div>
         </div>
 
         <div className="card-column two">
+
           <ul>
             <li>${props.price}/month</li>
             <li>{props.beds} bedrooms</li>
             <li>{props.proximity} mi. from campus</li>
           </ul>
+
+          <div className={props.available ? "availability-toggle available" : "availability-toggle unavailable"}>
+            {props.available ? "Available" : "Unavailable"}
+          </div>
+
         </div>
     </div>
   );
