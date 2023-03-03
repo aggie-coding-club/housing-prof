6f3bd95de1db44173228916d1653727846ed1a4a
kqwang123 <kwanggigme@tamu.edu>
2023-03-03 17:06:46 -0600

    DO NOT TOUCH

--- back/scraper/KevinStuff/boilerplating.js
+++ back/scraper/KevinStuff/boilerplating.js
@@ -9,6 +9,7 @@ const robotsParser = require('robots-parser');
 
 var url = "insert url here"; 
 
+// DON'T CHANGE
 function writeFile(data) {
   fs.writeFile ("data.json", JSON.stringify(data), function(err) {
       if (err) throw err;
@@ -16,69 +17,77 @@ function writeFile(data) {
   );
 }
 
+// DON'T CHANGE
 async function isNoIndex(page) {
-  const metaTags = await page.$$('meta');
-  for (const meta of metaTags) {
-    const name = await meta.getProperty('name');
-    const httpEquiv = await meta.getProperty('httpEquiv');
-    const content = await meta.getProperty('content');
-    const nameValue = await name.jsonValue();
-    const httpEquivValue = await httpEquiv.jsonValue();
-    const contentValue = await content.jsonValue();
-    if (nameValue === 'robots' && contentValue === 'noindex') {
-      return true;
+    const metaTags = await page.$$('meta');
+    for (const meta of metaTags) {
+        const name = await meta.getProperty('name');
+        const httpEquiv = await meta.getProperty('httpEquiv');
+        const content = await meta.getProperty('content');
+        const nameValue = await name.jsonValue();
+        const httpEquivValue = await httpEquiv.jsonValue();
+        const contentValue = await content.jsonValue();
+        if (nameValue === 'robots' && contentValue === 'noindex') {
+        return true;
+        }
+        if (httpEquivValue === 'X-Robots-Tag' && contentValue.includes('noindex')) {
+        return true;
+        }
     }
-    if (httpEquivValue === 'X-Robots-Tag' && contentValue.includes('noindex')) {
-      return true;
-    }
-  }
-  return false;
+    return false;
 }
 
 async function scrapeHousingPrices() {
-  const robotsUrl = `${new URL(url).origin}/robots.txt`;
-  const robotsTxt = await (await fetch(robotsUrl)).text();
-  const robots = robotsParser(robotsUrl, robotsTxt);
-  if (!robots.isAllowed(url, 'puppeteer')) {
-    console.log(`Cannot scrape ${url}: robots.txt disallows crawling`);
-    return false;
-  }
+    
+    // Robots parsing, DO NOT TOUCH
+    const robotsUrl = `${new URL(url).origin}/robots.txt`;
+    const robotsTxt = await (await fetch(robotsUrl)).text();
+    const robots = robotsParser(robotsUrl, robotsTxt);
+    if (!robots.isAllowed(url, 'puppeteer')) {
+        console.log(`Cannot scrape ${url}: robots.txt disallows crawling`);
+        return false;
+    }
 
-  const browser = await puppeteer.launch({headless : false});
-  const page = await browser.newPage();
+    // Opens page
+    const browser = await puppeteer.launch({headless : false});
+    const page = await browser.newPage();
 
-  if (await isNoIndex(page)) {
-    console.log(`Cannot scrape ${url}: page has noindex meta tag`);
+    // Parses noIndex, DO NOT TOUCH
+    if (await isNoIndex(page)) {
+        console.log(`Cannot scrape ${url}: page has noindex meta tag`);
+        await browser.close();
+        return false;
+    }
+
+    // opens page up
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    //const prices = await page.$$eval('div.detail-footerCost--reduced span', prices => prices.map(price => price.textContent));
+    //const rentLink = await page.$$eval('div.detail-footerApply a.detail-footerApplyLink', rentLink => rentLink.map(link => link.href));
+
+    // Edit code here:
+
+
+
+
+
+
+    
+    
+    // Get the timestamp of when it was scraped, DO NOT TOUCH
+    var today = new Date();
+    var timestamp = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    // Write json out to file, add elements as they are scraped
+    var json = {
+        // prices: prices,
+        // rent_link: rentLink,
+        time : timestamp
+    };
+
+    writeFile(json);
+
     await browser.close();
-    return false;
-  }
-
-  await page.goto(url, { waitUntil: 'networkidle2' });
-
-  //const prices = await page.$$eval('div.detail-footerCost--reduced span', prices => prices.map(price => price.textContent));
-  //const rentLink = await page.$$eval('div.detail-footerApply a.detail-footerApplyLink', rentLink => rentLink.map(link => link.href));
-
-
-
-
-
-
-
-
-  
-
-  var today = new Date();
-  var timestamp = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-  var json = {
-    // prices: prices,
-    // rent_link: rentLink,
-    time : timestamp
-  };
-
-  writeFile(json);
-
-  await browser.close();
 }
 
 scrapeHousingPrices();
