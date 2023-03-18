df18bfdfccf20688babf948d66c77e0e5d9056bb
kqwang123 <kwanggigme@tamu.edu>
2023-03-17 23:57:12 -0500

    Iframe compatible

--- scraper/KevinStuff/dynamictraceNestedHTML.js
+++ scraper/KevinStuff/dynamictraceNestedHTML.js
@@ -11,19 +11,13 @@ async function scrapeHousingPrices() {
 
   await page.goto(root, { waitUntil: 'networkidle2' });
 
-  const frame = await page.$("#website_1111144");
-    
-  if(frame){
-      // Wait for h2 inside the iframe
-      await frame.waitForSelector('head');
-  }else{
-       console.log("iFrame not found");
-  }
+  const frame = await page.$("#website_1111144"); 
+  const frameContent = await frame.contentFrame();
 
-  //const prices = await page.$$eval('span.amount', prices => prices.map(price => price.textContent));
-  //const rentLink = await page.$$eval('div.detail-footerApply a.detail-footerApplyLink', rentLink => rentLink.map(link => link.href));
-  //console.log(prices);
-  //console.log(rentLink);
+  const prices = await frameContent.$$eval('.rent-price', prices => prices.map(price => price.textContent));
+  const rentLink = await frameContent.$$eval('a.btn.availability', rentLink => rentLink.map(link => link.href));
+  console.log(prices);
+  console.log(rentLink);
 
   await browser.close();
 }
