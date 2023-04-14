4928eefe88274c632b50683c8d42acf18069080e
kqwang123 <kwanggigme@tamu.edu>
2023-04-14 13:49:13 -0500

    Create scrapemaster.js

---
+++ scraper/scrapemaster.js
@@ -0,0 +1,101 @@
+/*  This boilerplating is for a single website and is designed to scrape 
+    one or more components specifically for that site
+
+    Co-written by Chat-GPT
+*/ 
+
+// imports
+const fs = require('fs'); // file writer
+const puppeteer = require('puppeteer');
+const robotsParser = require('robots-parser');
+
+var url = "insert url here"; 
+
+// DON'T CHANGE
+function writeFile(data) {
+  fs.writeFile ("data.json", JSON.stringify(data), function(err) {
+      if (err) throw err;
+      }
+  );
+}
+
+// DON'T CHANGE
+async function isNoIndex(page) {
+    const metaTags = await page.$$('meta');
+    for (const meta of metaTags) {
+        const name = await meta.getProperty('name');
+        const httpEquiv = await meta.getProperty('httpEquiv');
+        const content = await meta.getProperty('content');
+        const nameValue = await name.jsonValue();
+        const httpEquivValue = await httpEquiv.jsonValue();
+        const contentValue = await content.jsonValue();
+        if (nameValue === 'robots' && contentValue === 'noindex') {
+        return true;
+        }
+        if (httpEquivValue === 'X-Robots-Tag' && contentValue.includes('noindex')) {
+        return true;
+        }
+    }
+    return false;
+}
+
+async function scrapeHousingPrices() {
+    
+    // Robots parsing, DO NOT TOUCH
+    const robotsUrl = `${new URL(url).origin}/robots.txt`;
+    const robotsTxt = await (await fetch(robotsUrl)).text();
+    const robots = robotsParser(robotsUrl, robotsTxt);
+    if (!robots.isAllowed(url, 'puppeteer')) {
+        console.log(`Cannot scrape ${url}: robots.txt disallows crawling`);
+        return false;
+    }
+
+    // Opens page
+    const browser = await puppeteer.launch({headless : false}); // set headless to true to remove browser
+    const page = await browser.newPage();
+
+    // Parses noIndex, DO NOT TOUCH
+    if (await isNoIndex(page)) {
+        console.log(`Cannot scrape ${url}: page has noindex meta tag`);
+        await browser.close();
+        return false;
+    }
+
+    // opens page up
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    // Get into the floor plans page
+
+    // For each floor plan
+
+        // For each room type
+
+            // Get price data
+            
+            // Get rent link
+            
+            // Get image links
+
+            // Get square feet
+
+            // Get address
+
+            // Get bedrooms
+            
+    
+    // Get the timestamp of when it was scraped, DO NOT TOUCH
+    var today = new Date();
+    var timestamp = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate() + ' ' + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    // Write json out to file, add elements as they are scraped
+    var json = {
+        time : timestamp
+    };
+
+    writeFile(json);
+
+    await browser.close();
+}
+
+scrapeHousingPrices();
+
